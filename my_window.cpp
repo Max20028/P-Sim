de06883aebf77bb157d6b92e05d979fbaa26c70e
Thomas B <tpburg8@gmail.com>
2022-05-21 16:09:37 -0700

    Picking Tutorial Complete
    So this tutorial was fairly easy. It is still kinda awkward as the cursor seems to default to a loading symbol due to the running nature of the program. Googling has shown no quick solution so this issue has been left for the future since usually you would not see the cursor

--- my_window.cpp
+++ my_window.cpp
@@ -100,7 +100,16 @@ XMVECTOR camUp;
 
 XMMATRIX cube1World;
 
-
+struct SurfaceMaterial
+{
+    std::wstring matName;
+    XMFLOAT4 difColor;
+    int texArrayIndex;
+    int normMapTexArrayIndex;
+    bool hasNormMap;
+    bool hasTexture;
+    bool transparent;
+};
 
 //------------------------
 //Time constants
@@ -141,6 +150,33 @@ float moveBackForward = 0.0f;
 float camYaw = 0.0f;
 float camPitch = 0.0f;
 //---------------------------
+//Picking
+std::vector<XMFLOAT3> groundVertPosArray;
+std::vector<DWORD> groundVertIndexArray;
+
+ID3D11Buffer* bottleVertBuff;
+ID3D11Buffer* bottleIndexBuff;
+std::vector<XMFLOAT3> bottleVertPosArray;
+std::vector<DWORD> bottleVertIndexArray;
+int bottleSubsets = 0;
+std::vector<int> bottleSubsetIndexStart;
+std::vector<int> bottleSubsetTexture;
+XMMATRIX bottleWorld[20];
+int* bottleHit = new int[20];
+int numBottles = 20;
+
+//Global Variables
+
+bool isShoot = false;
+
+int ClientWidth = 0;
+int ClientHeight = 0;
+
+int score = 0;
+float pickedDist = 0.0f;
+
+//---------------------------
+
 
 // function prototypes
 void InitD3D(HWND hwnd);     // sets up and initializes Direct3D
@@ -154,19 +190,29 @@ bool InitDirectInput(HINSTANCE hInstance, HWND hwnd);
 void DetectInput(double time, HWND hwnd);
 void UpdateCamera();
 //Define LoadObjModel function after we create surfaceMaterial structure
-bool LoadObjModel(std::wstring filename,        //.obj filename
-    ID3D11Buffer** vertBuff,            //mesh vertex buffer
-    ID3D11Buffer** indexBuff,            //mesh index buffer
-    std::vector<int>& subsetIndexStart,        //start index of each subset
+bool LoadObjModel(std::wstring filename,            //.obj filename
+    ID3D11Buffer** vertBuff,                    //mesh vertex buffer
+    ID3D11Buffer** indexBuff,                    //mesh index buffer
+    std::vector<int>& subsetIndexStart,            //start index of each subset
     std::vector<int>& subsetMaterialArray,        //index value of material for each subset
-    std::vector<struct SurfaceMaterial>& material,        //vector of material structures
-    int& subsetCount,                //Number of subsets in mesh
-    bool isRHCoordSys,                //true if model was created in right hand coord system
-    bool computeNormals);                //true to compute the normals, false to use the files normals
+    std::vector<SurfaceMaterial>& material,        //vector of material structures
+    int& subsetCount,                            //Number of subsets in mesh
+    bool isRHCoordSys,                            //true if model was created in right hand coord system
+    bool computeNormals,                        //true to compute the normals, false to use the files normals
+    std::vector<XMFLOAT3>& vertPosArray,        //Used for CPU to do calculations on the Geometry
+    std::vector<DWORD>& vertIndexArray);        //Also used for CPU caculations on geometry
 
 void StartTimer();
 double GetTime();
 double GetFrameTime();
+//Picking
+void pickRayVector(float mouseX, float mouseY, XMVECTOR& pickRayInWorldSpacePos, XMVECTOR& pickRayInWorldSpaceDir);
+float pick(XMVECTOR pickRayInWorldSpacePos,
+    XMVECTOR pickRayInWorldSpaceDir,
+    std::vector<XMFLOAT3>& vertPosArray,
+    std::vector<DWORD>& indexPosArray,
+    XMMATRIX& worldSpace);
+bool PointInTriangle(XMVECTOR& triV1, XMVECTOR& triV2, XMVECTOR& triV3, XMVECTOR& point );
 
 struct Vertex    //Overloaded Vertex Structure
 {
@@ -225,18 +271,10 @@ struct cbPerFrame
 
 cbPerFrame constbuffPerFrame;
 
-struct SurfaceMaterial
-{
-    std::wstring matName;
-    XMFLOAT4 difColor;
-    int texArrayIndex;
-    int normMapTexArrayIndex;
-    bool hasNormMap;
-    bool hasTexture;
-    bool transparent;
-};
+
 
 std::vector<SurfaceMaterial> material;
+std::vector<SurfaceMaterial> bottlematerial;
 
 
 // the WindowProc function prototype
@@ -333,6 +371,11 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+    case WM_SIZE:
+        ClientWidth  = LOWORD(lParam);
+        ClientHeight = HIWORD(lParam);
+        return 0;
+
     case WM_DESTROY:
         {
             PostQuitMessage(0);
@@ -345,7 +388,10 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 void drawstuff(std::wstring text, int inInt) {
         static const WCHAR sc_helloWorld[] = L"Hello, World!";
         std::wostringstream printString;
-        printString << text << inInt << "\nPitch: " << camPitch << "\nLightPos: " << light.pos.x << "," << light.pos.y << ", " << light.pos.z;
+        printString << text << inInt << "\nPitch: " << camPitch << "\n" 
+        << "LightPos: " << light.pos.x << "," << light.pos.y << ", " << light.pos.z << "\n"
+        << "Score: " << score << "\n"
+        << "Picked Dist: " << pickedDist;;
         std::wstring printText = printString.str();
 
         // Retrieve the size of the render target.
@@ -574,6 +620,9 @@ void CleanD3D(HWND hwnd) {
     meshVertBuff->Release();
     meshIndexBuff->Release();
 
+    bottleVertBuff->Release();
+    bottleIndexBuff->Release();
+
 }
 
 // this is the function used to render a single frame
@@ -633,6 +682,44 @@ void RenderFrame(void) {
         if(!material[meshSubsetTexture[i]].transparent)
             devcon->DrawIndexed( indexDrawAmount, indexStart, 0 );
     }
+
+    //draw bottle's nontransparent subsets
+    for(int j = 0; j < numBottles; j++)
+    {
+        if(bottleHit[j] == 0)
+        {
+            for(int i = 0; i < bottleSubsets; ++i)
+            {
+                //Set the grounds index buffer
+                devcon->IASetIndexBuffer( bottleIndexBuff, DXGI_FORMAT_R32_UINT, 0);
+                //Set the grounds vertex buffer
+                devcon->IASetVertexBuffers( 0, 1, &bottleVertBuff, &stride, &offset );
+
+                //Set the WVP matrix and send it to the constant buffer in effect file
+                WVP = bottleWorld[j] * camView * camProjection;
+                cbPerObj.WVP = XMMatrixTranspose(WVP);    
+                cbPerObj.World = XMMatrixTranspose(bottleWorld[j]);    
+                cbPerObj.difColor = bottlematerial[bottleSubsetTexture[i]].difColor;
+                cbPerObj.hasTexture = bottlematerial[bottleSubsetTexture[i]].hasTexture;
+                cbPerObj.hasNormMap = bottlematerial[bottleSubsetTexture[i]].hasNormMap;
+                devcon->UpdateSubresource( cbPerObjectBuffer, 0, NULL, &cbPerObj, 0, 0 );
+                devcon->VSSetConstantBuffers( 0, 1, &cbPerObjectBuffer );
+                devcon->PSSetConstantBuffers( 1, 1, &cbPerObjectBuffer );
+                if(bottlematerial[bottleSubsetTexture[i]].hasTexture)
+                    devcon->PSSetShaderResources( 0, 1, &meshSRV[bottlematerial[bottleSubsetTexture[i]].texArrayIndex] );
+                if(bottlematerial[bottleSubsetTexture[i]].hasNormMap)
+                    devcon->PSSetShaderResources( 1, 1, &meshSRV[bottlematerial[bottleSubsetTexture[i]].normMapTexArrayIndex] );
+                devcon->PSSetSamplers( 0, 1, &CubesTexSamplerState );
+
+                devcon->RSSetState(NoCullMode);
+                int indexStart = bottleSubsetIndexStart[i];
+                int indexDrawAmount =  bottleSubsetIndexStart[i+1] - bottleSubsetIndexStart[i];
+                if(!bottlematerial[bottleSubsetTexture[i]].transparent)
+                    devcon->DrawIndexed( indexDrawAmount, indexStart, 0 );
+            }
+        }
+    }
+
 
     //Draw our model's TRANSPARENT subsets now
 
@@ -885,7 +972,10 @@ void InitGraphics() {
 
     printf("L");
 
-    if(!LoadObjModel(L"resc/ground.obj", &meshVertBuff, &meshIndexBuff, meshSubsetIndexStart, meshSubsetTexture, material, meshSubsets, true, true)) return;
+    if(!LoadObjModel(L"resc/ground.obj", &meshVertBuff, &meshIndexBuff, meshSubsetIndexStart, meshSubsetTexture, material, meshSubsets, true, true, groundVertPosArray, groundVertIndexArray))
+        return;    
+    if(!LoadObjModel(L"resc/bottle.obj", &bottleVertBuff, &bottleIndexBuff, bottleSubsetIndexStart, bottleSubsetTexture, bottlematerial, bottleSubsets, true, true, bottleVertPosArray, bottleVertIndexArray))
+        return;
 
     printf("E\n");
     D3D11_SAMPLER_DESC sampDesc;
@@ -900,7 +990,34 @@ void InitGraphics() {
 
     dev->CreateSamplerState( &sampDesc, &CubesTexSamplerState );
 
-    
+
+    //Do all the setup for the bottles
+    float bottleXPos = -30.0f;
+    float bottleZPos = 30.0f;
+    float bxadd = 0.0f;
+    float bzadd = 0.0f;
+
+    for(int i = 0; i < numBottles; i++)
+    {
+        bottleHit[i] = 0;
+
+        //set the loaded bottles world space
+        bottleWorld[i] = XMMatrixIdentity();
+
+        bxadd++;
+
+        if(bxadd == 10)
+        {
+            bzadd -= 1.0f;
+            bxadd = 0;
+        }
+
+        Rotation = XMMatrixRotationY(3.14f);
+        Scale = XMMatrixScaling( 1.0f, 1.0f, 1.0f );
+        Translation = XMMatrixTranslation( bottleXPos + bxadd*10.0f, 4.0f, bottleZPos + bzadd*10.0f );
+
+        bottleWorld[i] = Rotation * Scale * Translation;
+    }
 }
 
 bool InitDirectInput(HINSTANCE hInstance, HWND hwnd) {
@@ -914,7 +1031,12 @@ bool InitDirectInput(HINSTANCE hInstance, HWND hwnd) {
     DIKeyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
 
     DIMouse->SetDataFormat(&c_dfDIMouse);
-    DIMouse->SetCooperativeLevel(hwnd, DISCL_EXCLUSIVE | DISCL_NOWINKEY | DISCL_FOREGROUND);
+
+    //Setup mouse
+    //This version has mouse invisible
+    // DIMouse->SetCooperativeLevel(hwnd, DISCL_EXCLUSIVE | DISCL_NOWINKEY | DISCL_FOREGROUND);
+    //This version has mouse visible. We want it visible for picking
+    DIMouse->SetCooperativeLevel(hwnd, DISCL_NONEXCLUSIVE | DISCL_NOWINKEY | DISCL_FOREGROUND);
 
     return true;
 }
@@ -953,6 +1075,58 @@ void DetectInput(double time, HWND hwnd)
     {
         moveBackForward -= speed;
     }
+
+    //Left Mouse Button
+    if(mouseCurrState.rgbButtons[0])
+    {
+        if(isShoot == false)
+        {    
+            POINT mousePos;
+
+            //This gets the position of the mouse on the monitor
+            GetCursorPos(&mousePos);   
+            //If it is windowed, this position is not necessarily bound to the window size. This corrects for that         
+            ScreenToClient(hwnd, &mousePos);
+
+            int mousex = mousePos.x;
+            int mousey = mousePos.y;        
+
+            float tempDist;
+            float closestDist = FLT_MAX;
+            int hitIndex;
+
+            XMVECTOR prwsPos, prwsDir;
+            pickRayVector(mousex, mousey, prwsPos, prwsDir);
+
+            for(int i = 0; i < numBottles; i++)
+            {
+                if(bottleHit[i] == 0) //No need to check bottles already hit
+                {
+                    tempDist = pick(prwsPos, prwsDir, bottleVertPosArray, bottleVertIndexArray, bottleWorld[i]);
+                    if(tempDist < closestDist)
+                    {
+                        closestDist = tempDist;
+                        hitIndex = i;
+                    }
+                }
+            }
+
+            if(closestDist < FLT_MAX)
+            {
+                bottleHit[hitIndex] = 1;
+                pickedDist = closestDist;
+                score++;
+            }
+
+            isShoot = true;
+        }
+    }
+    if(!mouseCurrState.rgbButtons[0])
+    {
+        isShoot = false;
+    }
+
+
     if((mouseCurrState.lX != mouseLastState.lX) || (mouseCurrState.lY != mouseLastState.lY))
     {
         camYaw += mouseLastState.lX * 0.001f;
@@ -1022,6 +1196,156 @@ void UpdateCamera()
     camView = XMMatrixLookAtLH( camPosition, camTarget, camUp );
 }
 
+
+void pickRayVector(float mouseX, float mouseY, XMVECTOR& pickRayInWorldSpacePos, XMVECTOR& pickRayInWorldSpaceDir)
+{
+    XMVECTOR pickRayInViewSpaceDir = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+    XMVECTOR pickRayInViewSpacePos = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+    float PRVecX, PRVecY, PRVecZ;
+
+    //Transform 2D pick position on screen space to 3D ray in View space
+    //Of note is that x and y could be set to zero and that would be center of screen, ie shooting in first person game, see next comment
+    //Also, because you can't directly access an XMMatrix, you have to copy the contents to a float4x4 first
+    XMFLOAT4X4 camProjTemp;
+    XMStoreFloat4x4(&camProjTemp, camProjection);
+    PRVecX =  ((( 2.0f * mouseX) / ClientWidth ) - 1 ) / camProjTemp(0,0);
+    PRVecY = -((( 2.0f * mouseY) / ClientHeight) - 1 ) / camProjTemp(1,1);
+    PRVecZ =  1.0f;    //View space's Z direction ranges from 0 to 1, so we set 1 since the ray goes "into" the screen
+
+    pickRayInViewSpaceDir = XMVectorSet(PRVecX, PRVecY, PRVecZ, 0.0f);
+
+    //Uncomment this line if you want to use the center of the screen (client area)
+    //to be the point that creates the picking ray (eg. first person shooter)
+    //pickRayInViewSpaceDir = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
+
+    // Transform 3D Ray from View space to 3D ray in World space
+    XMMATRIX pickRayToWorldSpaceMatrix;
+    XMVECTOR matInvDeter;    //We don't use this, but the xna matrix inverse function requires the first parameter to not be null
+
+    pickRayToWorldSpaceMatrix = XMMatrixInverse(&matInvDeter, camView);    //Inverse of View Space matrix is World space matrix
+
+    pickRayInWorldSpacePos = XMVector3TransformCoord(pickRayInViewSpacePos, pickRayToWorldSpaceMatrix);
+    pickRayInWorldSpaceDir = XMVector3TransformNormal(pickRayInViewSpaceDir, pickRayToWorldSpaceMatrix);
+}
+
+//This does the picking. The technique is actually very cool. Go look at the braynarzsoft tutorial for an explanation
+//The short version is you treat every triangle in the mesh as a plane and see where on that plane the ray intersects. Then see if that intersection point is inside triangle
+float pick(XMVECTOR pickRayInWorldSpacePos,
+    XMVECTOR pickRayInWorldSpaceDir, 
+    std::vector<XMFLOAT3>& vertPosArray,
+    std::vector<DWORD>& indexPosArray, 
+    XMMATRIX& worldSpace)
+{         
+    //Loop through each triangle in the object
+    for(int i = 0; i < indexPosArray.size()/3; i++)
+    {
+        //Triangle's vertices V1, V2, V3
+        XMVECTOR tri1V1 = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+        XMVECTOR tri1V2 = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+        XMVECTOR tri1V3 = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+        //Temporary 3d floats for each vertex
+        XMFLOAT3 tV1, tV2, tV3;
+
+        //Get triangle 
+        tV1 = vertPosArray[indexPosArray[(i*3)+0]];
+        tV2 = vertPosArray[indexPosArray[(i*3)+1]];
+        tV3 = vertPosArray[indexPosArray[(i*3)+2]];
+
+        tri1V1 = XMVectorSet(tV1.x, tV1.y, tV1.z, 0.0f);
+        tri1V2 = XMVectorSet(tV2.x, tV2.y, tV2.z, 0.0f);
+        tri1V3 = XMVectorSet(tV3.x, tV3.y, tV3.z, 0.0f);
+
+        //Transform the vertices to world space
+        tri1V1 = XMVector3TransformCoord(tri1V1, worldSpace);
+        tri1V2 = XMVector3TransformCoord(tri1V2, worldSpace);
+        tri1V3 = XMVector3TransformCoord(tri1V3, worldSpace);
+
+        //Find the normal using U, V coordinates (two edges)
+        XMVECTOR U = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+        XMVECTOR V = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+        XMVECTOR faceNormal = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+        U = tri1V2 - tri1V1;
+        V = tri1V3 - tri1V1;
+
+        //Compute face normal by crossing U, V
+        faceNormal = XMVector3Cross(U, V);
+
+        faceNormal = XMVector3Normalize(faceNormal);
+
+        //Calculate a point on the triangle for the plane equation
+        XMVECTOR triPoint = tri1V1;
+
+        //Get plane equation ("Ax + By + Cz + D = 0") Variables
+        float tri1A = XMVectorGetX(faceNormal);
+        float tri1B = XMVectorGetY(faceNormal);
+        float tri1C = XMVectorGetZ(faceNormal);
+        float tri1D = (-tri1A*XMVectorGetX(triPoint) - tri1B*XMVectorGetY(triPoint) - tri1C*XMVectorGetZ(triPoint));
+
+        //Now we find where (on the ray) the ray intersects with the triangles plane
+        float ep1, ep2, t = 0.0f;
+        float planeIntersectX, planeIntersectY, planeIntersectZ = 0.0f;
+        XMVECTOR pointInPlane = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+
+        ep1 = (XMVectorGetX(pickRayInWorldSpacePos) * tri1A) + (XMVectorGetY(pickRayInWorldSpacePos) * tri1B) + (XMVectorGetZ(pickRayInWorldSpacePos) * tri1C);
+        ep2 = (XMVectorGetX(pickRayInWorldSpaceDir) * tri1A) + (XMVectorGetY(pickRayInWorldSpaceDir) * tri1B) + (XMVectorGetZ(pickRayInWorldSpaceDir) * tri1C);
+
+        //Make sure there are no divide-by-zeros
+        if(ep2 != 0.0f)
+            t = -(ep1 + tri1D)/(ep2);
+
+        if(t > 0.0f)    //Make sure you don't pick objects behind the camera
+        {
+            //Get the point on the plane
+            planeIntersectX = XMVectorGetX(pickRayInWorldSpacePos) + XMVectorGetX(pickRayInWorldSpaceDir) * t;
+            planeIntersectY = XMVectorGetY(pickRayInWorldSpacePos) + XMVectorGetY(pickRayInWorldSpaceDir) * t;
+            planeIntersectZ = XMVectorGetZ(pickRayInWorldSpacePos) + XMVectorGetZ(pickRayInWorldSpaceDir) * t;
+
+            pointInPlane = XMVectorSet(planeIntersectX, planeIntersectY, planeIntersectZ, 0.0f);
+
+            //Call function to check if point is in the triangle
+            if(PointInTriangle(tri1V1, tri1V2, tri1V3, pointInPlane))
+            {
+                //Return the distance to the hit, so you can check all the other pickable objects in your scene
+                //and choose whichever object is closest to the camera
+                return t/2.0f;
+            }
+        }
+    }
+    //return the max float value (near infinity) if an object was not picked
+    return FLT_MAX;
+}
+
+bool PointInTriangle(XMVECTOR& triV1, XMVECTOR& triV2, XMVECTOR& triV3, XMVECTOR& point )
+{
+    //To find out if the point is inside the triangle, we will check to see if the point
+    //is on the correct side of each of the triangles edges.
+
+    XMVECTOR cp1 = XMVector3Cross((triV3 - triV2), (point - triV2));
+    XMVECTOR cp2 = XMVector3Cross((triV3 - triV2), (triV1 - triV2));
+    if(XMVectorGetX(XMVector3Dot(cp1, cp2)) >= 0)
+    {
+        cp1 = XMVector3Cross((triV3 - triV1), (point - triV1));
+        cp2 = XMVector3Cross((triV3 - triV1), (triV2 - triV1));
+        if(XMVectorGetX(XMVector3Dot(cp1, cp2)) >= 0)
+        {
+            cp1 = XMVector3Cross((triV2 - triV1), (point - triV1));
+            cp2 = XMVector3Cross((triV2 - triV1), (triV3 - triV1));
+            if(XMVectorGetX(XMVector3Dot(cp1, cp2)) >= 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    return false;
+}
+
 //This function is copied from the braynar tutorial. Since I wrote my own crappy one in old commit I decided to skip implementing and just copied
 bool LoadObjModel(std::wstring filename, 
     ID3D11Buffer** vertBuff, 
@@ -1031,7 +1355,9 @@ bool LoadObjModel(std::wstring filename,
     std::vector<SurfaceMaterial>& material, 
     int& subsetCount,
     bool isRHCoordSys,
-    bool computeNormals)
+    bool computeNormals,
+    std::vector<XMFLOAT3>& vertPosArray,
+    std::vector<DWORD>& vertIndexArray)
 {
     HRESULT hr = 0;
 
@@ -1803,7 +2129,13 @@ bool LoadObjModel(std::wstring filename,
         tempVert.texcoord = vertTexCoord[vertTCIndex[j]];
 
         vertices.push_back(tempVert);
+
+        //Copy just the vertex positions to the vector
+        vertPosArray.push_back(tempVert.pos);
     }
+
+    //Copy the index list to the array
+    vertIndexArray = indices;
 
     //////////////////////Compute Normals///////////////////////////
     //If computeNormals was set to true then we will create our own
